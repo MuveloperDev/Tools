5cfe0064c322f03b6d1367ff19153194c6a629c9
Muveloper <67819713+MuveloperDev@users.noreply.github.com>
2024-06-13 20:11:09 +0900

    Complete CustomHierarchy Custom tool

--- Assets/Scripts/Tools/CustomEditorWindows/HierarchyTool/CustomHierarchyElement.cs
+++ Assets/Scripts/Tools/CustomEditorWindows/HierarchyTool/CustomHierarchyElement.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEngine;
 
 public class CustomHierarchyElement : MonoBehaviour, ICustomHierarchyElement
@@ -16,3 +15,4 @@ public class CustomHierarchyElement : MonoBehaviour, ICustomHierarchyElement
         return InStyle;
     }
 }
+#endif
--- Assets/Scripts/Tools/CustomEditorWindows/HierarchyTool/ICustomHierarchyElement.cs
+++ Assets/Scripts/Tools/CustomEditorWindows/HierarchyTool/ICustomHierarchyElement.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR
 using UnityEngine;
 
+// Ensure to apply this feature only to objects used in the editor.
 public interface ICustomHierarchyElement
 {
     GUIStyle CustomHierarchyElementStyle(ref GUIStyle InStyle);
